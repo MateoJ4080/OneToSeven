6903e9e818be5f8b6f72f7713801e1a2367485a5
Mate93934 <mateodjm@gmail.com>
2025-01-25 18:51:37 -0300

    RigidBody implementation
    Basic Move() and RotateCamera() methods

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
     [SerializeField]
     private GameObject playerCamera;
     [SerializeField]
-    private float _speed;
+    private float _speed = 100;
+    [SerializeField] private float _camSens = 7;
+    private float _camRotationY = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,7 +19,8 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        Move();
+        RotateCamera();
     }
 
     private void Move()
@@ -25,6 +30,14 @@ public class Player : MonoBehaviour
         Vector3 forward = axisVertical * playerCamera.transform.forward * _speed * Time.deltaTime;
         Vector3 lateral = axisHorizontal * playerCamera.transform.right * _speed * Time.deltaTime;
         Vector3 increment = forward + lateral;
-        transform.GetComponent<Rigidbody>().MovePosition(transform.position + increment);
+        GetComponent<Rigidbody>().MovePosition(transform.position + increment);
+    }
+
+    private void RotateCamera()
+    {
+        float rotationX = playerCamera.transform.localEulerAngles.y + Input.GetAxis("Mouse X") * _camSens;
+        _camRotationY -= Input.GetAxis("Mouse Y") * _camSens;
+        _camRotationY = Mathf.Clamp(_camRotationY, -60, 60);
+        playerCamera.transform.localEulerAngles = new Vector3(_camRotationY, rotationX, 0);
     }
 }
