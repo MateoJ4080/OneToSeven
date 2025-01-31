8a154cd5d53f8233d31c7dd87fb45c9596d64d51
Mate93934 <mateodjm@gmail.com>
2025-01-31 20:57:38 -0300

    B
    B

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -3,19 +3,18 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     private int _coins;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
 
     }
     void AddCoins(int coins)
     {
-
+        Debug.Log("Picture");
     }
 }
